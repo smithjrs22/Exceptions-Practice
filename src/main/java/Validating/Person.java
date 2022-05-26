c4f7859775a38cd90ebc2332003c45428f9f08ba
9CD1KC2\admin <smithjrs22@gmail.com>
2022-05-26 14:57:13 -0400

    working on one but not finished

---
+++ src/main/java/Validating/Person.java
@@ -0,0 +1,34 @@
+package Validating;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        if (name == null){
+            throw new IllegalArgumentException("Name must not be null");
+        }
+        if (name.isEmpty()){
+            throw new IllegalArgumentException("Name must not be empty");
+        }
+        if (name.length() > 40){
+            throw new IllegalArgumentException("Name must not be over 40 characters in length");
+        }
+
+        if ( age < 0 || age > 120){
+
+            throw new IllegalArgumentException("Age must be between 0 and 120.");
+        }
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
